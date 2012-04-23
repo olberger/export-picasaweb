97964d9e73e2f019cf18975053198e27375b67ab
Olivier Berger <oberger@ouvaton.org>
2012-04-23 23:06:21 +0200

    Kinda alpha state : succesfully imported some photos

--- fetch.py
+++ fetch.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python
 
+#
+# This is a PicasaWeb photo exporter for OpenPhoto
+#
+# This program is Copyright (c) Olivier Berger <oberger@ouvaton.org>
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Code is heavily inspired by the flickr exporter
+
 # import os for file system functions
+import datetime
 import os
+import time
+import json
 
 import gdata.photos.service
 
@@ -21,11 +43,77 @@ def fetch(login, app_password, albumid = None):
                                                                album.numphotos.text, album.gphoto_id.text)
     else :
         username = 'default'
+        albumentry = gd_client.GetFeed(
+            '/data/feed/api/user/%s/albumid/%s' % (
+                username, albumid))
+        #print albumentry
+        #print 'Access:', albumentry.access.text
+        permission = 0
+        if albumentry.access.text == 'public':
+            permission = 1
+
         photos = gd_client.GetFeed(
             '/data/feed/api/user/%s/albumid/%s?kind=photo' % (
                 username, albumid))
+
         for photo in photos.entry:
-            print 'Photo title:', photo.title.text
+
+            p = {}
+
+            photoid = photo.gphoto_id.text
+
+            p['id'] = photoid
+            p['permission'] = bool(permission)
+            p['title'] = photo.title.text
+            #p['license'] = 
+            
+            description = photo.summary.text
+            if description is not None:
+                p['description'] = description
+
+            where = photo.geo.Point.pos.text
+            if where :
+                (latitude, longitude) = where.split(' ')
+                p['latitude'] = float(latitude)
+                p['longitude'] = float(longitude)
+
+            tagsl = []
+            tags = gd_client.GetFeed(photo.GetTagsUri())
+            for tag in tags.entry:
+                tagsl.append(tag.title.text)
+            if tagsl :
+                p['tags'] = tagsl
+            
+            epoch = float(photo.timestamp.text)/1000
+            p['dateTaken'] = int(time.mktime(time.gmtime(epoch)))
+            #print 'dateTaken:', photo.timestamp.isoformat()
+            p['dateUploaded'] = int(time.mktime(time.strptime(photo.published.text, '%Y-%m-%dT%H:%M:%S.000Z')))
+            
+            p['photo'] = photo.media.content[0].url
+            #print 'AlbumID:', photo.albumid.text
+            # if photo.exif.make and photo.exif.model:
+            #     camera = '%s %s' % (photo.exif.make.text, photo.exif.model.text)
+            # else:
+            #     camera = 'unknown'
+            # print 'Camera:', camera
+            # print 'First Thumbnail:', photo.media.thumbnail[0].url
+            # if photo.commentCount.text != '0' :
+            #     comments = gd_client.GetFeed(photo.GetCommentsUri())
+            #     for comment in comments.entry:
+            #         print 'Comment', comment.content.text
+            #print photo
+            
+            #print p
+
+            t = datetime.datetime.fromtimestamp(float(p['dateUploaded']))
+            filename = '%s-%s' % (t.strftime('%Y%m%dT%H%M%S'), p['id'])
+
+            print "  * Storing photo %s to fetched/%s.json" % (p['id'], filename),
+            f = open("fetched/%s.json" % filename, 'w')
+            f.write(json.dumps(p))
+            f.close()
+            print "OK"
+
 
 # create a directory only if it doesn't already exist
 def createDirectorySafe( name ):
